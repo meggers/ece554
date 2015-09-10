d59b0a3a122421550a1ad432d301d5af565b810f
Max Eggers <eggers.max@gmail.com>
2015-09-09 21:09:54 -0500

    first pass receive module

--- project1/io.v
+++ project1/io.v
@@ -28,6 +28,8 @@ module transmit(
             if (~iorw & tbr) begin
                 shiftReg <= {1'b0, data, 1'b1};
                 tbr <= 1'b0;
+
+            // if we are in the middle of writing
             end else if (~tbr) begin
 
                 // shift out data
@@ -40,6 +42,8 @@ module transmit(
                 end else begin
                     tbr <= tbr;
                 end
+
+            // I don't know what state this is
             end else begin
                 tbr <= tbr;
                 out <= out;
@@ -65,8 +69,49 @@ module receive(
     input enable,
     input [1:0] ioaddr,
 
-    output rda,
-    output [7:0] data
+    output reg rda,
+    output reg [7:0] data
     );
+
+    reg [9:0] shiftReg; // two extra bits for start and stop
+
+    always @(posedge clk) begin
+
+        // reset
+        if (rst) begin
+            shiftReg <= 10'b1;
+            data <= 8'b0;
+            rda <= 1'b0;
+
+        // read operation is occuring
+        end else if (iorw) begin
+            rda <= 1'b0;
+            shiftReg <= 10'b1;
+            data <= data;
+
+        // read baud
+        end else if (enable) begin
+
+            // we have received a full byte
+            if (~shiftReg[0] & shiftReg[9]) begin
+                data <= shiftReg;
+                shiftReg <= 10'b1;
+                rda <= 1'b1;
+
+            // we are still reading
+            end else begin
+                data <= data;
+                rda <= rda;
+                shiftReg <= {in, shiftReg[9:1]};
+            end
+
+        // idle
+        end else begin
+            shiftReg <= shiftReg;
+            data <= data;
+            rda <= rda;
+        end
+
+    end
 
 endmodule
